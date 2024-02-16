c325c949d83aeff1e7135668c2335157e4283acc
Frank Schuerer <frank.schuerer@thyssenkrupp.com>
2024-02-16 17:05:53 +0100

    Multiple issuers

--- appserver/payara-appserver-modules/microprofile/jwt-auth/src/main/java/fish/payara/microprofile/jwtauth/eesecurity/SignedJWTIdentityStoreMutliIssuers.java
+++ appserver/payara-appserver-modules/microprofile/jwt-auth/src/main/java/fish/payara/microprofile/jwtauth/eesecurity/SignedJWTIdentityStoreMutliIssuers.java
@@ -1,8 +1,14 @@
 package fish.payara.microprofile.jwtauth.eesecurity;
 
-import com.nimbusds.jwt.SignedJWT;
-import static fish.payara.microprofile.jwtauth.eesecurity.SignedJWTIdentityStore.readVendorProperties;
-import jakarta.security.enterprise.identitystore.CredentialValidationResult;
+import static org.eclipse.microprofile.jwt.config.Names.ISSUER;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,15 +18,24 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
-import static org.eclipse.microprofile.jwt.config.Names.ISSUER;
+
+import com.nimbusds.jwt.SignedJWT;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 
 /**
  *
  * @author XLKAFR
  */
 public class SignedJWTIdentityStoreMutliIssuers extends SignedJWTIdentityStore {
+
+    private static final Logger LOGGER = Logger.getLogger(SignedJWTIdentityStoreMutliIssuers.class.getName());
 
     protected List<String> acceptedIssuers;
     protected Map<String, JwtPublicKeyStore> issuer2PublicKeyStore;
@@ -39,8 +54,11 @@ public class SignedJWTIdentityStoreMutliIssuers extends SignedJWTIdentityStore {
 
         for (String issuer : acceptedIssuers) {
             Optional<Properties> properties = readVendorProperties();
-            var ks = new JwtPublicKeyStore(readPublicKeyCacheTTL(properties), Optional.of(issuer + "/certs"));
-            issuer2PublicKeyStore.put(issuer, ks);
+            String jwksUri = getJwksUri(issuer);
+            if (jwksUri != null) {
+                var ks = new JwtPublicKeyStore(readPublicKeyCacheTTL(properties), Optional.of(jwksUri));
+                issuer2PublicKeyStore.put(issuer, ks);
+            }
         }
     }
 
@@ -61,9 +79,44 @@ public class SignedJWTIdentityStoreMutliIssuers extends SignedJWTIdentityStore {
                 setAcceptedIssuer(issuer);
                 setPublicKeyStore(issuer2PublicKeyStore.get(issuer));
             } catch (ParseException ex) {
-                Logger.getLogger(SignedJWTIdentityStoreMutliIssuers.class.getName()).log(Level.SEVERE, null, ex);
+                LOGGER.log(Level.SEVERE, null, ex);
             }
         }
         return super.validate(signedJWTCredential);
+    }
+
+    private static String getJwksUri(String issuer) {
+        String jwksUri = null;
+        try {
+            URI issuerURI = new URI(issuer);
+            String pathConfig = "";
+            if (!issuerURI.getPath().endsWith("/")) {
+                pathConfig += "/";
+            }
+            pathConfig += ".well-known/openid-configuration";
+            URI providerConfigurationURI = issuerURI.resolve(issuerURI.getPath() + pathConfig);
+
+            HttpRequest request = HttpRequest.newBuilder(providerConfigurationURI).GET().build();
+            HttpClient client = HttpClient
+                    .newBuilder()
+                    .sslContext(NoOpSSLContextBuilder.build())
+                    .build();
+            HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+            if (response.statusCode() == 200) {
+                try (InputStream inputStream = response.body(); JsonReader reader = Json.createReader(inputStream)) {
+                    JsonObject config = reader.readObject();
+                    if (config.containsKey("jwks_uri")) {
+                        jwksUri = config.getString("jwks_uri");
+                    } else {
+                        LOGGER.log(Level.SEVERE, "Property 'jwks_uri' not found in openid-configuration from Issuer: {0}", providerConfigurationURI.toString());
+                    }
+                };
+            } else {
+                LOGGER.log(Level.SEVERE, "Cannot load openid-configuration from Issuer: {0} status code: {1}", new Object[]{providerConfigurationURI.toString(), response.statusCode()});
+            }
+        } catch (URISyntaxException | IOException | InterruptedException ex) {
+            LOGGER.log(Level.SEVERE, "Error loading public certificate from Issuer " + issuer, ex);
+        }
+        return jwksUri;
     }
 }
